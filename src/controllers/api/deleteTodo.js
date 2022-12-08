29f70a8f5b906bc988fb483b2acb88adac8e594f
Andy Little <69594249+andy-little@users.noreply.github.com>
2022-12-08 13:11:36 +0000

    added delete endpoint

--- src/controllers/api/deleteTodo.js
+++ src/controllers/api/deleteTodo.js
@@ -1,3 +1,17 @@
-const deleteTodo = (req, res) => res.send('deleteTodo');
+import Todo from '../../db/models/Todo.js';
+
+const deleteTodo = async (req, res) => {
+  try {
+    await Todo.findOneAndDelete({
+      userId: req.userId,
+      _id: req.params.id,
+    });
+    return res
+      .status(204)
+      .json({ message: `successfully deleted task with id ${req.params.id}` });
+  } catch (error) {
+    return res.status(400).json({ error: error.message });
+  }
+};
 
 export default deleteTodo;
--- src/controllers/api/getTodo.js
+++ src/controllers/api/getTodo.js
@@ -3,7 +3,7 @@ import Todo from '../../db/models/Todo.js';
 const getTodo = async (req, res) => {
   try {
     const todo = await Todo.findOne({ userId: req.userId, _id: req.params.id });
-    if (todo?._id) return res.status(200).json({ todo });
+    if (todo) return res.status(200).json({ todo });
     return res.status(404).json({ error: 'Item not found' });
   } catch (error) {
     return res.status(400).json({ error: error.message });
