7ccaee523a0fe0921c532725b41b93e675d06089
Andy Little <69594249+andy-little@users.noreply.github.com>
2022-12-08 13:19:15 +0000

    added update endpoint

--- src/controllers/api/updateTodo.js
+++ src/controllers/api/updateTodo.js
@@ -1,3 +1,17 @@
-const createTodo = (req, res) => res.send('createTodo');
+import Todo from '../../db/models/Todo.js';
 
-export default createTodo;
+const updateTodo = async (req, res) => {
+  try {
+    const todo = await Todo.findOneAndUpdate(
+      { userId: req.userId, _id: req.params.id },
+      req.body,
+      { new: true, runValidators: true }
+    );
+    if (todo) return res.status(200).json({ todo });
+    return res.status(404).json({ error: 'Item not found' });
+  } catch (error) {
+    return res.status(400).json({ error: error.message });
+  }
+};
+
+export default updateTodo;
