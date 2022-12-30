0ee74ed835a5ae4582abd67f175ef0cd6fac661a
andy-little <69594249+andy-little@users.noreply.github.com>
2022-12-30 08:59:36 +0000

    added jwt to register and login

--- src/db/models/User.js
+++ src/db/models/User.js
@@ -1,5 +1,9 @@
+import * as dotenv from 'dotenv';
 import mongoose from 'mongoose';
 import bcryptjs from 'bcryptjs';
+import jwt from 'jsonwebtoken';
+
+dotenv.config();
 
 const UserSchema = new mongoose.Schema({
   fName: {
@@ -37,4 +41,20 @@ UserSchema.pre('save', async function hashPassword() {
     await bcryptjs.genSalt(12)
   );
 });
+
+UserSchema.methods.createJWT = function createJWT() {
+  return jwt.sign(
+    { userId: this._id, userName: this.email },
+    process.env.JWT_SECRET,
+    {
+      expiresIn: process.env.JWT_EXPIRY,
+    }
+  );
+};
+
+UserSchema.methods.isAuthorized = async function isAuthorized(password) {
+  const res = await bcryptjs.compare(password, this.password);
+  return res;
+};
+
 export default mongoose.model('users', UserSchema);
