c169cc687ec88dceba90d1ac731332b483d5588a
andy-little <69594249+andy-little@users.noreply.github.com>
2022-12-31 10:41:17 +0000

    switched tests back to esm

---
+++ src/controllers/api/__tests__/apiTodo.test.js
@@ -0,0 +1,19 @@
+// const createTodo = require('../createTodo');
+
+describe('createTodo', () => {
+  it('should run tests', () => {
+    // const req = {
+    //   body: {
+    //     name: 'test name',
+    //     description: 'test desc',
+    //     status: 'idle',
+    //   },
+    // };
+    // const result = createTodo(req);
+    // console.log(result);
+    expect(4).toBe(4);
+  });
+});
+
+/* check error handling */
+/* check model called with currect args */
